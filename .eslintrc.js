e5fdef365c90e92e42267d5ff2de46730bf038da
Devabubakar <abubakarali3w@gmail.com>
2023-01-24 19:20:20 +0300

    updated: original line endings in my working directory

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   rules: {
     'import/prefer-default-export': 'off',
     'vue/multi-word-component-names': 'off',
